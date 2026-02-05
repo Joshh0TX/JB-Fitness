af7b8ed631bcb58b2c9a7ccb1c4aeeceaf8a499e
Joshua Omotoso <jae5omotoso@gmail.com>
2026-02-05 22:09:31 +0100

    changed DB_HOST to MYSQL_URL and linked to env on Render

--- jbfitness-backend/src/config/db.js
+++ jbfitness-backend/src/config/db.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.MYSQL_URL,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
