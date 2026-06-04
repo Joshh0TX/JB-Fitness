5734d70360c10b340a1d8e906ab35d0da7171c62
Joshua Omotoso <jae5omotoso@gmail.com>
2026-06-04 16:30:16 +0100

    removed other things

--- backend/src/modules/exercise/exercises.routes.js
+++ backend/src/modules/exercise/exercises.routes.js
@@ -5,9 +5,4 @@ import { searchExercises } from "./exercises.controller.js";
 const router = express.Router();
 router.post("/search", authMiddleware, searchExercises);
 
-
-router.get("/all", authMiddleware, async (req, res) => {
-  const exercises = await prisma.exercise.findMany({ take: 5 });
-  res.json(exercises);
-});
 export default router;
